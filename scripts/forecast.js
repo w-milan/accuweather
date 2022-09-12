9d613cb73c17a7ab92cdf78524dca9d092cd9838
milan <pperisic.milan@gmail.com>
2022-09-12 23:35:57 +0200

    Added City API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = 'Ff7PkHZwqkdYYGL8S9Z4GHTRRxrwvCAc';
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+getCity('Glasgow')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
