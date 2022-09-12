234a962742f370d39055df4b6937a99dcf74dca4
milan <pperisic.milan@gmail.com>
2022-09-13 00:08:30 +0200

    Added Weather API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,20 @@
 const key = 'Ff7PkHZwqkdYYGL8S9Z4GHTRRxrwvCAc';
 
+// weather
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+
+// city
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +27,9 @@ const getCity = async (city) => {
 
 };
 
-getCity('Glasgow')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('Glasgow').then(data => {
+    return getWeather(data.Key);
+  }).then(data => {
+    console.log(data);
+  }).catch(err => console.log(err));
+
