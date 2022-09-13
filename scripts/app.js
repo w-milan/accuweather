a67005c8f87f55fc446b0a24504e7cd3663e8743
milan <pperisic.milan@gmail.com>
2022-09-13 13:25:10 +0200

    Added function to make values on display

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,33 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
+
+};
 
 const updateCity = async (city) => {
 
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
 
-  return {
-    cityDets: cityDets,
-    weather: weather
-  };
+  return { cityDets, weather };
 
 };
 
@@ -20,7 +39,7 @@ cityForm.addEventListener('submit', e => {
   cityForm.reset();
 
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 
 });
