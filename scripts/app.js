caa9cd6da0c1f96fc46a2af0fdc05ed83999fdb9
milan <pperisic.milan@gmail.com>
2022-09-13 14:53:12 +0200

    Added images and making of day/night and icons on display

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
   const cityDets = data.cityDets;
   const weather = data.weather;
 
+  // update details template
   details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
@@ -15,6 +18,18 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
   `;
+
+  // icon image and day/night image
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = 'img/day.svg'
+  } else {
+    timeSrc = 'img/night.svg'
+  }
+  time.setAttribute('src', timeSrc);
 
   if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
