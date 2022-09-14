738896a82fa89fa7b2fca73776113fdde0735f17
milan <pperisic.milan@gmail.com>
2022-09-14 22:14:53 +0200

    Deleted settings

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -27,9 +27,3 @@ const getCity = async (city) => {
 
 };
 
-// getCity('Glasgow').then(data => {
-//     return getWeather(data.Key);
-//   }).then(data => {
-//     console.log(data);
-//   }).catch(err => console.log(err));
-
